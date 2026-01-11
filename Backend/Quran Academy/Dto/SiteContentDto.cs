220a00bfb7f1411110a27fd96ed33a3a30e81afd
Omarmagdy66 <105433708+Omarmagdy66@users.noreply.github.com>
2026-01-12 01:08:46 +0200

    Delete Backend directory

--- Backend/Quran Academy/Dto/SiteContentDto.cs
+++
@@ -1,9 +0,0 @@
-namespace Dto
-{
-    public class SiteContentDto
-    {
-        public string? Email { get; set; }
-        public string? Phone { get; set; }
-        public string? Password { get; set; }
-    }
-}
